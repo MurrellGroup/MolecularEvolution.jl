7e45cffc69f5e92581ca1de8389fd318f7eb5f43
Maximilian Danielsson <maximilian.danielsson@gmail.com>
2024-04-18 15:40:23 +0200

    Get rid of .temp and implement

--- src/models/lazy_partition.jl
+++ src/models/lazy_partition.jl
@@ -9,25 +9,33 @@
 export LazyPartition
 mutable struct LazyPartition{PType} <: Partition where {PType <: Partition}
     partition::Union{PType, Nothing}
-    temp::Union{PType, Nothing} #This is a temporary memory location for the message passing wave.
-    memoryblocks::Vector{PType} #A stack that is meant for LazyPartitions to share
+    memoryblocks::Vector{PType} #A stack that is meant for LazyPartitions in the same tree to share
 
     function LazyPartition{PType}(partition) where {PType <: Partition}
-        new(partition, deepcopy(partition)) #Maybe there is a better way to get a new instance of the same type?
+        new(partition, Vector{PType}())
     end
+
+    function LazyPartition{PType}(partition, memoryblocks) where {PType <: Partition}
+        new(partition, memoryblocks)
+    end
+end
+
+function copy_partition(src::LazyPartition{PType}) where {PType}
+    partition = isnothing(src.partition) ? nothing : copy_partition(src.partition)
+    return LazyPartition(partition, src.memoryblocks) #We want to have a reference to the same stack
 end
 
 function combine!(dest::LazyPartition{PType}, src::LazyPartition{PType}) where {PType}
     (isnothing(src.partition) || isnothing(dest.partition)) && throw(ArgumentError("The partition field in both the source and destination LazyPartitions must be defined to combine them."))
     combine!(dest.partition, src.partition)
     push!(src.memoryblocks, src.partition)
-    src.partition, src.temp = nothing, nothing
+    src.partition = nothing
 end
 
 
 """
 The idea with backward!{LazyPartition} is to avoid having to call the GC too often,
-therefore, we always need a temp memory location in the message passing wave. This is only really necessary if PType <: MultisitePartition,
+therefore, we need access to shared memoryblocks during the message passing wave. This is only really necessary if PType <: MultisitePartition,
 but for consistency, we might as well do it for all types?
 """
 
@@ -37,9 +45,9 @@ I store:
     - Sequences at leafnodes
 
 To achieve a maximum amount of memoryblocks available during felsenstein!, the workflow of backward! and combine! is this:
-    - We pop a memoryblock of the stack and put it in source.temp prior to a backward!.
+    - We pop a memoryblock of the stack and put it in dest.partition prior to a backward!.
     Now, we perform the backward!, then push our source.partition to the memoryblock stack
-    - We let our temps be nothing during the combine! ops as they are irrelevant here.
+    - After a combine!, we push src.partition to the memoryblock stack
 """
 
 function backward!(
@@ -53,12 +61,11 @@ function backward!(
         lazy_obs2partition!(source.partition, node.node_data["obs"])
         # In the case of CodonPartition, we need to enforce scaling being zeros (which we do with lazy_obs2partition)
     end
-    source.temp = pop!(source.memoryblocks)
-    (isnothing(source.partition) || isnothing(source.temp)) && throw(ArgumentError("The partition and temp fields in the source LazyPartition must be defined to propagate it backwards."))
-    backward!(source.temp, source.partition, model, node)
+    dest.partition = pop!(source.memoryblocks)
+    (isnothing(source.partition) || isnothing(dest.partition)) && throw(ArgumentError("The partition field in the source and dest LazyPartition must be something to propagate it backwards."))
+    backward!(dest.partition, source.partition, model, node)
     push!(source.memoryblocks, source.partition)
-    dest.partition = source.temp
-    source.partition, source.temp = nothing, nothing
+    source.partition = nothing
 end
 
 function site_LLs(dest::LazyPartition)
@@ -66,12 +73,12 @@ function site_LLs(dest::LazyPartition)
 end
 
 function copy_partition_to!(dest::LazyPartition{PType}, src::LazyPartition{PType}) where {PType}
-    dest.partition, dest.temp = src.partition, src.temp
-    src.partition, src.temp = nothing, nothing #Hmm, we might not always want to wipe the src?
+    dest.partition = src.partition
+    src.partition = nothing #Hmm, we might not always want to wipe the src?
 end
 
 """
-Sorts for a minimal count of active partitions during a felsenstein!
+Should be run on a tree containing LazyPartitions before running `felsenstein!`. Sorts for a minimal count of active partitions during a felsenstein!
 Note: since felsenstein! uses a stack, we want to avoid having long node.children[1].children[1]... chains
 """
 function lazysort!(node)
@@ -95,6 +102,7 @@ function populate_tree!(
     data;
     init_all_messages = true,
     tolerate_missing = 1, #0 = error if missing; 1 = warn and set to missing data; 2 = set to missing data
+    leaf_name_transform = x -> x
 )
     if init_all_messages
         internal_message_init!(tree, starting_message)
@@ -104,7 +112,7 @@ function populate_tree!(
     name_dic = Dict(zip(names, 1:length(names)))
     for n in getleaflist(tree)
         if haskey(name_dic, n.name)
-            n.node_data = Dict("obs" => data[name_dic[n.name]])
+            n.node_data = Dict("obs" => data[name_dic[leaf_name_transform(n.name)]])
         else
             warn_str = n.name * " on tree but not found in names."
             if tolerate_missing == 0
@@ -138,6 +146,5 @@ function internal_message_init!(tree::FelNode, empty_message::LazyPartition{T}) 
     end
 end
 
-#Need a faster realization of the leafnodes that avoids allocating new matrices and vectors...
-function lazy_obs2partition!(dest::LazyPartition{PType}, seq) where {PType}
-end
+#TODO Get `copy_partition` working, then I will only have to implement copy_partition(src::LazyPartition)
+#TODO Figure out how to generalize `populate_message` for LazyPartition, as we actually want to store the seqs in node_data
