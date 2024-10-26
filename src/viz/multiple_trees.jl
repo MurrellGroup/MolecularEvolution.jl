33f5bbc01ff885bf2d20b5b4c3b8d929fae77e15
nossleinad <maximilian.danielsson@gmail.com>
2024-10-26 17:12:22 +0200

    WLS: safety commit

--- src/viz/multiple_trees.jl
+++ src/viz/multiple_trees.jl
@@ -20,6 +20,7 @@ function getdistfromrootdict(node::FelNode, xdict::Dict{FelNode, Float64} = Dict
     return xdict
 end
 
+#TODO: handle a = 0
 function WLS_δ(tree, inf_tree, weight_fn)
     a, b = 0.0, 0.0
     xdict, inf_xdict = getdistfromrootdict(tree), getdistfromrootdict(inf_tree)
@@ -36,20 +37,82 @@ function WLS_δ(tree, inf_tree, weight_fn)
     return -b / (2*a)
 end
 
+function WLS_linalg(tree, inf_tree, weight_fn; opt_scale = true)
+    xdict, inf_xdict = getdistfromrootdict(tree), getdistfromrootdict(inf_tree)
+    matched_pairs = tree_match_pairs(tree, inf_tree, push_leaves = true)
+    #Make sure root is added to the matched_pairs, but not twice...
+    all(x -> !isroot(x[1]), matched_pairs) && push!(matched_pairs, (tree, inf_tree))
+    n = length(matched_pairs)
+    w, x, y = zeros(n), zeros(n), zeros(n)
+    for (i, (node, inf_node)) in enumerate(matched_pairs)
+        @assert (w[i] = weight_fn(node)) >= 0
+        x[i] = xdict[node]
+        y[i] = inf_xdict[inf_node]
+    end
+    if !opt_scale
+        location, scale = sum(w .* (y - x)) / sum(w), 1.0
+    else
+        sqrt_w = sqrt.(w)
+        location, scale = (hcat(sqrt_w, sqrt_w .* x)) \ (sqrt_w .* y)
+    end
+    return location, scale
+end
+
+#Consider just returning the optimized scale parameter when things go sideways
+function WLS(tree, inf_tree, weight_fn; opt_scale = true)
+    a, b, c, d, e = 0.0, 0.0, 0.0, 0.0, 0.0
+    xdict, inf_xdict = getdistfromrootdict(tree), getdistfromrootdict(inf_tree)
+    matched_pairs = tree_match_pairs(tree, inf_tree, push_leaves = true)
+    #Make sure root is added to the matched_pairs, but not twice...
+    all(x -> !isroot(x[1]), matched_pairs) && push!(matched_pairs, (tree, inf_tree))
+    #Weigh the square dists of matched pairs
+    for (node, inf_node) in matched_pairs
+        @assert (w = weight_fn(node)) >= 0
+        a += w
+        b += w * xdict[node]
+        c += w * inf_xdict[inf_node]
+        d += w * xdict[node]^2
+        e += w * xdict[node] * inf_xdict[inf_node]
+    end
+    if a == 0.0
+        @warn "the weights are all identically 0"
+        #[location, scale]
+        return [0.0, 1.0]
+    end
+    #location_opt = [location, 1.0]
+    location_opt = [(c - b) / a, 1.0]
+    if !opt_scale
+        return location_opt
+    end
+    try
+        #[location, scale]
+        location, scale = [a b; b d] \ [c, e]
+        #TODO: make sure scale isn't < 0
+        return location, scale
+    catch e
+        if isa(e, LinearAlgebra.SingularException)
+            @warn "singular matrix encountered"
+            return location_opt #TODO: return global minimum
+        else
+            rethrow(e)
+        end
+    end
+end
+
 # Function to recursively plot internal nodes and edges
-function plot_internal!(node, x_parent, line_width, line_alpha, flag, attr)
+function plot_internal!(node, x_parent, scale, line_width, line_alpha, flag, attr)
     if isleafnode(node)
         y = attr.y_coords[node.name]
-        x = x_parent + node.branchlength
+        x = x_parent + node.branchlength * scale
         if flag
             Plots.scatter!([x], [y], markersize=attr.node_size, color=:blue)
             Plots.annotate!(x + attr.margin, y, Plots.text(node.name, attr.font_size, :left))
         end
         return x, y
     else
+        node_x = x_parent + node.branchlength * scale
         children = node.children
-        child_coords = [plot_internal!(child, x_parent + node.branchlength, line_width, line_alpha, flag, attr) for child in children]
-        node_x = x_parent + node.branchlength
+        child_coords = [plot_internal!(child, node_x, scale, line_width, line_alpha, flag, attr) for child in children]
         node_y = sum(last.(child_coords)) / length(child_coords)
 
         for (child_x, child_y) in child_coords
@@ -83,7 +146,8 @@ function plot_multiple_trees(trees, inf_tree;
     font_size=10,
     margin=1.0,
     line_alpha=0.05,
-    weight_fn=n::FelNode -> ifelse(isroot(n), 1.0, 0.0))
+    weight_fn=n::FelNode -> ifelse(isroot(n), 1.0, 0.0),
+    opt_scale=true)
     # Assume all trees have the same leaf set
  
     leaves = getleaflist(inf_tree)
@@ -98,9 +162,9 @@ function plot_multiple_trees(trees, inf_tree;
  
  
     # Find the maximum x-coordinate (tree depth) across all trees
-    tbl = totalbl(inf_tree)
+    target_scale = target_total_bl / totalbl(inf_tree)
     for n in getnodelist(inf_tree)
-        n.branchlength *= target_total_bl/tbl
+        n.branchlength *= target_scale
     end
     #max_depth = maximum(values(node_distances(inf_tree)))
 
@@ -109,19 +173,27 @@ function plot_multiple_trees(trees, inf_tree;
  
     # Plot all trees
     for tree in trees
-        tbl = totalbl(tree)
-        for n in getnodelist(tree)
-            n.branchlength *= target_total_bl/tbl
-        end
         ladderize!(tree)
-        δ = WLS_δ(tree, inf_tree, weight_fn)
-        plot_internal!(tree, δ, line_width, line_alpha, false, attr)
+        location, scale = WLS(tree, inf_tree, weight_fn; opt_scale = opt_scale)
+        location_la, scale_la = WLS_linalg(tree, inf_tree, weight_fn; opt_scale = opt_scale)
+        if !(location ≈ location_la)
+            @show location, location_la, opt_scale
+        end
+        if !(scale ≈ scale_la)
+            @show scale, scale_la, opt_scale
+        end
+        @assert location ≈ location_la
+        @assert scale ≈ scale_la
+        plot_internal!(tree, location, scale, line_width, line_alpha, false, attr)
     end
  
     ladderize!(inf_tree)
-    plot_internal!(inf_tree, 0, 2.0, 1.0, true, attr)
+    plot_internal!(inf_tree, 0.0, 1.0, 2.0, 1.0, true, attr)
  
     Plots.ylims!(0, n_leaves + 1)
  
+    for n in getnodelist(inf_tree)
+        n.branchlength /= target_scale
+    end
     return p
 end
